b61ebf177a1dc8d8afe21b98c0eea1581628862c
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-04 14:18:04 +0200

    Updated the styling, check mark is used to check off todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,33 +1,45 @@
 // Check off specific todos by clicking
 
-$("ul").on("click", "li", function () {
-    $(this).toggleClass("completed");
+$("ul").on("click", "#check", function (e) {
+  $(this)
+    .parent()
+    .toggleClass("completed");
+  $("#check i").toggleClass("fa-check fa-check-double");
+
+  //   $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
 
-$("ul").on("click", "span", function (e) {
-    $(this).parent().fadeOut(500, function () {
-        $(this).remove();
-    })
-    e.stopPropagation();
+$("ul").on("click", "#trash", function (e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function () {
+      $(this).remove();
+    });
+  e.stopPropagation();
 });
 
 $("input[type='text']").keypress(function (e) {
-    if (e.which === 13) {
-        var todoText = $(this).val();
-        if (!(todoText.length > 35)) {
-            $(this).css("border", "3px solid rgba(0, 0, 0, 0)");
-            $("ul").append("<li><span> <i class='fas fa-trash-alt'></i> </span>" + todoText + "</li>")
-            $(this).val("");
-        } else $(this).css("border", "3px solid red");
-    }
+  if (e.which === 13) {
+    var todoText = $(this).val();
+    console.log(todoText.length);
+    if (!(todoText.length > 35))
+      if (todoText.length > 0) {
+        $(this).css("border", "3px solid rgba(0, 0, 0, 0)");
+        $("ul").append(
+          "<li><span id='check'> <i class='fas fa-check'></i></span>" +
+          todoText +
+          "<span id='trash'><i class='fas fa-trash-alt'></i></span></li>"
+        );
+        $(this).val("");
+      } else $(this).css("border", "3px solid red");
+  }
 });
 
-
 $("#icon").on("click", function () {
-    $("input[type='text']").slideToggle(200, function () {
-        $("#icon").toggleClass("fa-arrow-down fa-arrow-up");
-    });
-    $("li").slideToggle();
-})
+  $("input[type='text']").slideToggle(200, function () {
+    $("#icon").toggleClass("fa-arrow-down fa-arrow-up");
+  });
+  $("li").slideToggle();
+});
